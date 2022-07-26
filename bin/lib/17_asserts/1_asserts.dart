d13d8383fc692b32cff76d7af01059b7da5fc880
Gabriel Donato <gabrieldonato@brasilcard.net>
2022-07-26 10:36:29 -0300

    Feat(Asserts): estudos sobre asserts

---
+++ bin/lib/17_asserts/1_asserts.dart
@@ -0,0 +1,13 @@
+import 'cliente.dart';
+
+//SO FUNCIONA EM TEMPO DE DESENVOLVIMENTO
+//EM PROD O  APP TRAVA.
+
+void main() {
+  var cli = Cliente(
+    nome: '',
+    cpf: '111.111.111-11',
+    cnpj: '1123123213213',
+    razaoSocial: 'fisica',
+  );
+}
---
+++ bin/lib/17_asserts/cliente.dart
@@ -0,0 +1,26 @@
+class Cliente {
+  String? nome;
+  String? cpf;
+
+  String? razaoSocial;
+  String? cnpj;
+  Cliente({
+    this.nome,
+    this.cpf,
+    this.razaoSocial,
+    this.cnpj,
+  })  : assert(
+          (cpf != null)
+              ? nome != null && razaoSocial == null && cnpj == null
+              : true,
+          'Você enviou cpf junto com cnpj',
+        ),
+        assert(
+          (cnpj != null)
+              ? razaoSocial != null && nome == null && cpf == null
+              : true,
+          'Você enviou cnpj junto com cpf',
+        );
+  // assert(nome != null, 'Nome não pode ser nulo'),
+  // assert(cpf != null, 'Cpf não pode ser nulo');
+}
